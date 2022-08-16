507d80cae9830022dede64e66cd5a14fb88c1b46
EnTheos <theosenigma@gmail.com>
2022-08-16 18:05:35 +0300

    mini slider

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -1,13 +1,48 @@
+import { useRef, useState } from 'react';
+
 import adidasLogo from './image/adidas_logo.png';
 import arrowNext from './image/arrow_next.svg';
 import './slider.scss'
 
 export default function Slider() {
+    const [currentSlide, setCurrentSlide] = useState(1);
+    const sliderTape = useRef(null);
+
+    const maxCurrentSlide = 2;
+    
+    function onSwitchSlide() {
+        setCurrentSlide((prev) => {
+            if (maxCurrentSlide === prev) {
+                return 0
+            }
+            return prev + 1;
+        });
+        sliderTape.current.style.transform = `translateX(${-(currentSlide * 960)}px)`;
+    }
+
     return (
         <section className='slider'>
             <div className="limit">
                 <div className="sliderWindow">
-                    <div className="sliderTape">
+                    <div className="sliderTape" ref={sliderTape}>
+                        <div className="slide">
+                            <div className='logo'>
+                                <img src={adidasLogo} alt="adidasLogo" />
+                            </div>
+                            <div className='slogan'>
+                                <h2><span>Stan Smith</span>, <br /> Forever!</h2>
+                                <button>Купить</button>
+                            </div>
+                        </div>
+                        <div className="slide">
+                            <div className='logo'>
+                                <img src={adidasLogo} alt="adidasLogo" />
+                            </div>
+                            <div className='slogan'>
+                                <h2><span>Stan Smith</span>, <br /> Forever!</h2>
+                                <button>Купить</button>
+                            </div>
+                        </div>
                         <div className="slide">
                             <div className='logo'>
                                 <img src={adidasLogo} alt="adidasLogo" />
@@ -19,7 +54,7 @@ export default function Slider() {
                         </div>
                     </div>
                 </div>
-                <button className="buttonNext">
+                <button onClick={onSwitchSlide} className="buttonNext">
                     <img src={arrowNext} alt="arrowNext" />
                 </button>
             </div>
