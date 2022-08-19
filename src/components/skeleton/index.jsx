5f9adcbcb4327ea6005c92ce80910a0d24f2a91c
EnTheos <theosenigma@gmail.com>
2022-08-19 13:15:51 +0300

    updating skeleton

--- src/components/skeleton/index.jsx
+++ src/components/skeleton/index.jsx
@@ -1,13 +1,25 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
 import './skeleton.scss';
 
 export default function Skeleton() {
     return (
         <div className="cardSkelet">
-            <div className="imgSkelet"></div>
-            <div className='bigNameSkelet'></div>
-            <div className='smallNameSkelet'></div>
-            <div className='priceSkelet'></div>
-            <div className='addSkelet'></div>
+            <ContentLoader
+                speed={2}
+                width={150}
+                height={200}
+                viewBox="0 0 150 200"
+                backgroundColor="#f3f3f3"
+                foregroundColor="#ecebeb"
+            >
+                <rect x="0" y="0" rx="10" ry="10" width="150" height="91" />
+                <rect x="0" y="95" rx="3" ry="3" width="150" height="15" />
+                <rect x="0" y="166" rx="3" ry="3" width="80" height="24" />
+                <rect x="0" y="115" rx="0" ry="0" width="93" height="15" />
+                <rect x="114" y="158" rx="8" ry="8" width="32" height="32" />
+            </ContentLoader>
         </div>
     )
 }
